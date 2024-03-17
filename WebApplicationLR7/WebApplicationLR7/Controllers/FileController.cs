cc33501307d79ea44f29cb4d1ce9e2fca20565a8
UkrNicolasCage <93522793+UkrNicolasCage@users.noreply.github.com>
2024-03-18 00:46:32 +0200

    Add files via upload

---
+++ WebApplicationLR7/WebApplicationLR7/Controllers/FileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationLR7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DownloadFile(string firstName, string lastName, string fileName)
+        {
+           
+            var contentInFile = $"My name: {firstName} and surname: {lastName}";
+            var pathToFile = Path.Combine(Directory.GetCurrentDirectory(), $"{fileName}.txt");
+            await System.IO.File.WriteAllTextAsync(pathToFile, contentInFile, Encoding.UTF8);
+
+            return PhysicalFile(pathToFile, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
